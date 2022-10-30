0b193fcf565958f9c5a93ab45d23ebb866afc120
Sai Teja <saiteja.pamulapati@mail.concordia.ca>
2022-10-30 12:17:16 -0400

    added logs

--- src/utils/user.js
+++ src/utils/user.js
@@ -2,13 +2,11 @@ import { doGet, doPost } from "./webUtils"
 
 const baseUrl = "https://unified-server.herokuapp.com"
 
-
 export const getUserDetails = async (email) => {
     return await doGet(baseUrl + "/unified/v1/users?emailId="+ email)
 }
 
 export const saveUserDetails = async (body) => {
-    console.log("saving details");
-    console.log("body",body)
+    console.log(`saving user details body - ${JSON.stringify(body)}`);
     return await doPost(baseUrl + "/unified/v1/users", body);
 }
