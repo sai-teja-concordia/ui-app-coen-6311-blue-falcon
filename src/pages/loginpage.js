994c05999ac2630c25244041c193a082e9ef8abc
Sri-vadlamudi <astrosrikanth5@gmail.com>
2022-11-06 20:25:58 -0500

    11th commit

--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,10 +1,11 @@
 
-import background from '../static/background_1.jpg';
+import background from '../static/background_3.jpg';
 import '../App.css';
 import {LoginButton} from "../components/login";
 
 import { useEffect} from 'react';
 import { gapi } from 'gapi-script';
+import { Form } from 'react-router-dom';
 
 
 const clientId="257151779582-a1360hlmj2h92fuv79albkgrf9j6qn20.apps.googleusercontent.com";
@@ -24,19 +25,30 @@ function LoginPage() {
 
   return (
     
-      <header   >
-        <img src={background} className="loginbackground "width="1400" height="600" alt="background"/>
+      <div className='loginpage'>
         
-        <p className="loginpara">
-          Welcome to
-        </p>
-        <p className="loginpara2">
-          Unified social Media App
-        </p>
-        <div className="loginbutton">
-          <LoginButton />
-        </div>
-      </header>      
+        <img src={background} className="loginbackground "width="1400" height="600" alt="background"/>
+          <form className='loginbox'>
+            <div className="loginbutton">
+              <LoginButton />
+            </div>
+            
+            
+          </form>
+          <text className="loginpara">
+              Welcome to
+            </text>
+            <text className="loginpara2">
+               Unified social Media
+            </text>
+            <text className="loginpara3">
+               To stay connected
+            </text>
+            
+        
+
+        
+      </div>      
     
     
   );
