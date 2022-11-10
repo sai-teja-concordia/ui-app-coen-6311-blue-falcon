733bc306dbec296f08cc4672ad20c2c6b718c4ce
Sri-vadlamudi <astrosrikanth5@gmail.com>
2022-11-10 14:19:45 -0500

    carousel trail

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,61 @@
-import React from 'react';
-import Header from '../components/Header';
+import React from "react";
+import Header from "../components/Header";
 import countries from "countries-list";
 
-import {Navigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import Carousel from "react-elastic-carousel";
+import Item from "../components/items";
 
+let welcomeMessage;
 
-let welcomeMessage
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
+];
 
-function Home() {    
-  console.log(`localStorage.getItem('isNewUser') - ${localStorage.getItem('isNewUser')}`);
-  if (localStorage.getItem('isNewUser') === 'false') {
-      welcomeMessage ="Welcome back! "
-    }
-    else  {
-      welcomeMessage= "Welcome, "
+function Home() {
+  
+    console.log(
+      `localStorage.getItem('isNewUser') - ${localStorage.getItem("isNewUser")}`
+    );
+    if (localStorage.getItem("isNewUser") === "false") {
+      welcomeMessage = "Welcome back! ";
+    } else {
+      welcomeMessage = "Welcome, ";
       // return <Navigate to="/Newuser" />
     }
-    welcomeMessage = welcomeMessage + localStorage.getItem('name')
-    console.log(welcomeMessage)
+    welcomeMessage = welcomeMessage + localStorage.getItem("name");
+    console.log(welcomeMessage);
     console.log(countries.countries);
+  
 
-  return (
-    <div>
-      <Header></Header>
-      <div className='content'><h1>{welcomeMessage}</h1></div>
+  
+    return (
+      <div>
+        <Header></Header>
+        <div className="content">
+          <h1>{welcomeMessage}</h1>
+        </div>
+        <div className="Carousel">
+          
+        <Carousel breakPoints={breakPoints}>
+          <Item>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+          <Item>Four</Item>
+          <Item>Five</Item>
+          <Item>Six</Item>
+          <Item>Seven</Item>
+          <Item>Eight</Item>
+        </Carousel>
+      </div>
 
-      
-       
         
-        
-      
-    </div>
-  );
-}
+      </div>
+    );
+    }
 
 
 export default Home;
