df757c45bfc96dc8864b3a10ec514619b1b08fd5
Sri-vadlamudi <astrosrikanth5@gmail.com>
2022-11-13 15:47:54 -0500

    header css changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "../components/Header";
 
-
 // import { Navigate } from "react-router-dom";
 import Carousel from "react-elastic-carousel";
 import Item from "../components/items";
 import Item2 from "../components/items2";
 import background from "../static/background_4.jpg";
-
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
@@ -16,21 +14,18 @@ const breakPoints = [
   { width: 1200, itemsToShow: 2 },
 ];
 
-
 function Home() {
-  
-
-
-
   let listOfNews = [
     {
       id: "id",
-      title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      title:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       url: "https://www.google.com",
       urlToImage: background,
       author: "Srikanth",
       description: "description",
-      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       publishedAtEpoch: 1668192430,
       sourceName: "sourceName1",
     },
@@ -82,16 +77,119 @@ function Home() {
   let category_func = [
     {
       category: "Sports",
+      listofnews: [
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+      ],
     },
     {
       category: "Science",
+      listofnews: [
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+      ],
     },
     {
       category: "Hollywood",
-    }
-
+      listofnews: [
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+        {
+          id: "5",
+          title: "sports1",
+          url: "https://www.google.com",
+          urlToImage: background,
+          author: "Srikanth",
+          description: "description",
+          content: "content",
+          publishedAtEpoch: 1668192430,
+          sourceName: "sourceName1",
+        },
+      ],
+    },
   ];
-
 
   let categorynews = (
     <div>
@@ -102,27 +200,28 @@ function Home() {
           </div>
 
           <Carousel breakPoints={breakPoints}>
-          {listOfNews.map((item) => (
-            <Item>
-              <Item2 id={item.id}>
-                <img src={item.urlToImage} width="250" height="150" alt=""></img>
-                <text  className= "news-content">{item.content}</text>
-              </Item2>
+            {cat.listofnews.map((item) => (
+              <Item>
+                <Item2 id={item.id}>
+                  <img
+                    src={item.urlToImage}
+                    width="250"
+                    height="150"
+                    alt=""
+                  ></img>
+                  <text className="news-content">{item.content}</text>
+                </Item2>
 
-              <Item2  id={item.id}>
-                 <text  className= "news-title">{item.title}</text>
-                                 
-                <a href={item.url}>Read more</a>
-              </Item2>
-            </Item>
-          ))}
-        </Carousel>
+                <Item2 id={item.id}>
+                  <text className="news-title">{item.title}</text>
+
+                  <a href={item.url}>Read more</a>
+                </Item2>
+              </Item>
+            ))}
+          </Carousel>
         </div>
-
       ))}
-
-
-
     </div>
   );
   // const listItems = trendingnews.map((trendingnews) =>
@@ -144,13 +243,18 @@ function Home() {
           {listOfNews.map((item) => (
             <Item>
               <Item2 id={item.id}>
-                <img src={item.urlToImage} width="250" height="150" alt=""></img>
-                <text  className= "news-content">{item.content}</text>
+                <img
+                  src={item.urlToImage}
+                  width="250"
+                  height="150"
+                  alt=""
+                ></img>
+                <text className="news-content">{item.content}</text>
               </Item2>
 
-              <Item2  id={item.id}>
-                 <text  className= "news-title">{item.title}</text>
-                                 
+              <Item2 id={item.id}>
+                <text className="news-title">{item.title}</text>
+
                 <a href={item.url}>Read more</a>
               </Item2>
             </Item>
