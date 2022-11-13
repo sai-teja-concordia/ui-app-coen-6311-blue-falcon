f419e0c272acbaa162171067b096346641f974cb
Sri-vadlamudi <astrosrikanth5@gmail.com>
2022-11-13 17:59:17 -0500

    api call succesfull

--- src/components/login.js
+++ src/components/login.js
@@ -1,85 +1,82 @@
-import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin } from "react-google-login";
 import { Navigate } from "react-router-dom";
-import React from 'react';
-import { getUserDetails } from '../utils/user';
-import { saveUserDetails } from '../utils/user';
+import React from "react";
+import { getUserDetails } from "../utils/user";
+import { saveUserDetails } from "../utils/user";
+import { getTrendingnews } from "../utils/newsUtils";
 
-const clientId = "257151779582-a1360hlmj2h92fuv79albkgrf9j6qn20.apps.googleusercontent.com";
+const clientId =
+  "257151779582-a1360hlmj2h92fuv79albkgrf9j6qn20.apps.googleusercontent.com";
 
-let responseFromServer
+let responseFromServer;
+let responsefromnewsserver;
+
 function LoginButton() {
-    const [logstat, setlogstat] = React.useState(false);
-    const onSucess = async (response) => {
+  const [logstat, setlogstat] = React.useState(false);
+  const onSucess = async (response) => {
+    let profile = response.profileObj;
+    console.log("response", response);
+    console.log("Google Login Sucess! Current user:", profile);
+    responseFromServer = await getUserDetails(profile.email);
+    console.log("response from server: ", responseFromServer);
 
-        let profile = response.profileObj;
-        console.log("response", response);
-        console.log("Google Login Sucess! Current user:", profile);
-        responseFromServer = await getUserDetails(profile.email)
-        console.log("response from server: ", responseFromServer)
+    if (responseFromServer.data) {
+      console.log("old user");
+      console.log(responseFromServer.data);
+      localStorage.setItem("isNewUser", false);
+      localStorage.setItem("name", responseFromServer.data.name);
+      localStorage.setItem("id", responseFromServer.data.id);
+      localStorage.setItem("country", responseFromServer.data.location);
+      responsefromnewsserver = await getTrendingnews(
+        responseFromServer.data.location
+      );
+      console.log("response from news server: ", responsefromnewsserver);
 
-
-        if (responseFromServer.data) {
-            console.log("old user");
-            console.log(responseFromServer.data);
-            localStorage.setItem('isNewUser', false);
-            localStorage.setItem('name', responseFromServer.data.name)
-            localStorage.setItem('id', responseFromServer.data.id)
-            localStorage.setItem('country', responseFromServer.data.location)
-            // localStorage.setItem()
-        } else {
-            console.log("new user");
-            console.log(responseFromServer.data);
-            localStorage.setItem('isNewUser', true);
-            let user = profile
-            user.emailId = profile.email
-            console.log(profile);
-            let responseToServer = await saveUserDetails(user)
-            console.log("saved details response: ", responseToServer)
-            localStorage.setItem('name', profile.name)
-            localStorage.setItem('id', responseToServer.data.id)
-        }
-        console.log(localStorage);
-        localStorage.setItem('email', profile.email)
-        localStorage.setItem('familyName', profile.familyName)
-        localStorage.setItem('givenName', profile.givenName)
-        localStorage.setItem('imageUrl', profile.imageUrl)
-        setlogstat(true)
+      // localStorage.setItem()
+    } else {
+      console.log("new user");
+      console.log(responseFromServer.data);
+      localStorage.setItem("isNewUser", true);
+      let user = profile;
+      user.emailId = profile.email;
+      console.log(profile);
+      let responseToServer = await saveUserDetails(user);
+      console.log("saved details response: ", responseToServer);
+      localStorage.setItem("name", profile.name);
+      localStorage.setItem("id", responseToServer.data.id);
     }
-    const onFailure = (res) => {
-        console.log("LOGIN FAILED! res", res);
+    console.log(localStorage);
+    localStorage.setItem("email", profile.email);
+    localStorage.setItem("familyName", profile.familyName);
+    localStorage.setItem("givenName", profile.givenName);
+    localStorage.setItem("imageUrl", profile.imageUrl);
+    setlogstat(true);
+  };
+  const onFailure = (res) => {
+    console.log("LOGIN FAILED! res", res);
+  };
+  console.log("logstat - ", logstat);
+  if (logstat) {
+    console.log("Navigating to home");
+    if (responseFromServer.data) {
+      return <Navigate to="/Home" />;
+    } else {
+      return <Navigate to="/Newuser" />;
     }
-    console.log('logstat - ', logstat)
-    if (logstat) {
-        console.log("Navigating to home")
-        if (responseFromServer.data) {
-            return <Navigate to="/Home" />
-        }
-        else {
-            return <Navigate to="/Newuser" />
-        }
+  }
 
-
-    }
-
-    return (
-
-        <div id="signInButton">
-            <GoogleLogin
-
-                theme='dark'
-                clientId={clientId}
-                buttonText="Login with Google"
-                onSuccess={onSucess}
-                onFailure={onFailure}
-                cookiePolicy={'single_host_origin'}
-                isSignedIn={true}
-
-            />
-
-        </div>
-
-    )
-
-
+  return (
+    <div id="signInButton">
+      <GoogleLogin
+        theme="dark"
+        clientId={clientId}
+        buttonText="Login with Google"
+        onSuccess={onSucess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+      />
+    </div>
+  );
 }
 export { LoginButton };
--- src/utils/newsUtils.js
+++ src/utils/newsUtils.js
@@ -1,1 +1,9 @@
 // {baseUrl}/unified/v1/news/trending?country={canada}
+import { doGet, doPatch, doPost } from "./webUtils";
+
+const baseUrl = "https://unified-server.herokuapp.com";
+// const baseUrl = "http://localhost:8080"
+
+export const getTrendingnews = async (country) => {
+  return await doGet(baseUrl + "/unified/v1/news/trending?country=" + country);
+};
