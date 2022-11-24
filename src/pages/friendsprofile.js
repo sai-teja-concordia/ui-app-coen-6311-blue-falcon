1c8c4abe2173defe5396a24b9fe9de54f6057960
Sri-vadlamudi <astrosrikanth5@gmail.com>
2022-11-23 20:58:59 -0500

    friends profile page details

--- src/pages/friendsprofile.js
+++ src/pages/friendsprofile.js
@@ -2,12 +2,54 @@ import React from "react";
 import Header from "../components/Header";
 import { TextField } from "@mui/material";
 
+import { useEffect, useState } from "react";
+import { getUserfriends } from "../utils/user";
+
 function Friends() {
+  const [Userfriends, setUserFriends] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    let userId = localStorage.getItem("id");
+    console.log("userid - ");
+    console.log(userId);
+
+    getUserfriends(userId).then((response) => {
+      if (mounted && response.data) {
+        setUserFriends(response.data);
+      }
+
+      localStorage.setItem("friends", response.data);
+    });
+
+    return () => (mounted = false);
+  }, []);
+
+  const listItems = Userfriends.map((names) => (
+    <li>
+      <a>{names.name}</a>{" "}
+      <img src={names.imageUrl} width="50" height="50"></img>
+      <li>
+        {" "}
+        <a>{names.emailId}</a>
+      </li>
+      <li>
+        <a>{names.location}</a>
+      </li>
+    </li>
+  ));
+
+  let frnd = localStorage.getItem("friends");
+  // let frndid = localStorage.getItem("friendid");
+  console.log("f:");
+  console.log(frnd);
+
   return (
     <div>
       <div>
         <Header></Header>
       </div>
+
       <div className="main">
         <div className="search">
           <TextField
@@ -17,6 +59,10 @@ function Friends() {
             label="Search"
           />
         </div>
+      </div>
+
+      <div className="friends-list">
+        <ul>{listItems}</ul>
       </div>
     </div>
   );
